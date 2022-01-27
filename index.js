d10ac140cffba2b0215125f8cce9e887b5e61bcc
hiren parghi <hirenparghi90@gmail.com>
2022-01-27 19:31:46 +0530

    latest

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
   const response = {
     statusCode: 200,
-    body: JSON.stringify("Hello from 1Lambda and Github!"),
+    body: JSON.stringify("Hello all from 1Lambda and Github!"),
   }
   return response
 }
